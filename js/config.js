61bbe589b97acdec15b78344c45ef8541e40e797
AlekseyNaruto <tushkov.aleksey11@gmail.com>
2026-03-25 18:40:13 +0500

    Delete config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Конфигурация без токена! Токен будет подставлен при деплое
-window.GITHUB_CONFIG = {
-    owner: 'AlekseyNaruto',
-    repo: 'AlekseyNaruto.github.io',
-    // token добавляется через GitHub Actions!
-};
